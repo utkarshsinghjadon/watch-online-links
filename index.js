bb8f5c725aeaca4cb6e4ec6767b156ba4611e150
Utkarsh Singh <64301885+utkarshsinghjadon@users.noreply.github.com>
2021-03-02 15:36:08 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var docRef = db.collection("Hotstar").doc("livetelecast");
+docRef.get().then((doc) => {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch((error) => {
+    console.log("Error getting document:", error);
+});
+
