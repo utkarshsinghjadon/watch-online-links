ae89ba424c76ea39d62140976d04f72b1d867b2d
Utkarsh Singh <64301885+utkarshsinghjadon@users.noreply.github.com>
2021-03-02 16:49:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,27 @@ docRef.get().then((doc) => {
       const li = `
       
         <a href="${theory.E01}">E01</a>
+        <hr>
+        <a href="${theory.E01}">E02</a>
         <video playsinline controls src="${theory.E02}">E02</video>
+        <hr>
+          <a href="${theory.E01}">E03</a>
         <video playsinline controls src="${theory.E03}">E03</video>
+        <hr>
+          <a href="${theory.E01}">E04</a>
         <video playsinline controls src="${theory.E04}">E04</video>
+        <hr>
+          <a href="${theory.E01}">E05</a>
         <video playsinline controls src="${theory.E05}">E05</video>
+        <hr>
+          <a href="${theory.E01}">E06</a>
         <video playsinline controls src="${theory.E06}">E06</video>
+        <hr>
+          <a href="${theory.E01}">E07</a>
         <video playsinline controls src="${theory.E07}">E07</video>
-<!--    <video playsinline controls src="${theory.E08}">E08</video>-->
+        <!--  <hr>
+          <a href="${theory.E01}">E08</a>
+  <video playsinline controls src="${theory.E08}">E08</video>-->
 
       `;
       html += li
