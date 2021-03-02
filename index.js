e7ca064ee333e4b4a3fb5d3b4e16c09b16ea3885
Utkarsh Singh <64301885+utkarshsinghjadon@users.noreply.github.com>
2021-03-02 16:58:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,25 +9,25 @@ docRef.get().then((doc) => {
         <a href="${theory.E01}">E01</a>
         <hr>
         <a href="${theory.E01}">E02</a>
-        <video playsinline controls src="${theory.E02}">E02</video>
+        <video playsinline controls src="${theory.e02}">E02</video>
         <hr>
           <a href="${theory.E01}">E03</a>
-        <video playsinline controls src="${theory.E03}">E03</video>
+        <video playsinline controls src="${theory.e03}">E03</video>
         <hr>
           <a href="${theory.E01}">E04</a>
-        <video playsinline controls src="${theory.E04}">E04</video>
+        <video playsinline controls src="${theory.e04}">E04</video>
         <hr>
           <a href="${theory.E01}">E05</a>
-        <video playsinline controls src="${theory.E05}">E05</video>
+        <video playsinline controls src="${theory.e05}">E05</video>
         <hr>
           <a href="${theory.E01}">E06</a>
-        <video playsinline controls src="${theory.E06}">E06</video>
+        <video playsinline controls src="${theory.e06}">E06</video>
         <hr>
           <a href="${theory.E01}">E07</a>
-        <video playsinline controls src="${theory.E07}">E07</video>
+        <video playsinline controls src="${theory.e07}">E07</video>
         <!--  <hr>
           <a href="${theory.E01}">E08</a>
-  <video playsinline controls src="${theory.E08}">E08</video>-->
+  <video playsinline controls src="${theory.E08}">e08</video>-->
 
       `;
       html += li
