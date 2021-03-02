b4dad5def281c77fd45554a4c167c500a505cc3d
Utkarsh Singh <64301885+utkarshsinghjadon@users.noreply.github.com>
2021-03-02 16:36:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ docRef.get().then((doc) => {
         let html = '';
     const theory = doc.data();
       const li = `
-      <li>
-        <div class="collapsible-header grey lighten-4">${theory.E01}</div>
-        <div class="collapsible-body white">${theory.E02}</div>
-          
-        </li>
+      <h1>Hotstar Live Telecast Season 1</h1>
+        <a href="${theory.E01}">E01</a>
+        <a href="${theory.E02}">E02</a>
+        <a href="${theory.E03}">E03</a>
+        <a href="${theory.E04}">E04</a>
+        <a href="${theory.E05}">E05</a>
+        <a href="${theory.E06}">E06</a>
+        <a href="${theory.E07}">E07</a>
+<!--    <a href="${theory.E08}">E08</a>-->
+
       `;
       html += li
         guideList.innerHTML = html;
